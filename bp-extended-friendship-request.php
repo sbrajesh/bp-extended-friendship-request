e132394e572eaf13a64f263ff69600d91cda3f6a
PressProgrammer <dev@pressprogrammer.com>
2025-08-16 16:51:59 +0530

    update version

--- bp-extended-friendship-request.php
+++ bp-extended-friendship-request.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: BuddyPress Extended Friendship Request
- * Version: 1.2.1
+ * Version: 1.2.2
  * Plugin URI: https://buddydev.com/plugins/bp-extended-friendship-request/
  * Author: BuddyDev Team
  * Author URI: https://buddydev.com
